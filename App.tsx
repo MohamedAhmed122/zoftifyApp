f519eff4ecc352e07235413c0e1a1e7255cbbb6f
JS_Ninja <mohamed.ahm9833@gmail.com>
2022-08-10 09:30:49 +0700

    Test husky

--- App.tsx
+++ App.tsx
@@ -1,7 +1,10 @@
 import {StyleSheet, Text, View} from 'react-native';
+import compose from 'use-compose';
 import React from 'react';
 
 export const App = () => {
+  console.log('A7a');
+
   return (
     <View>
       <Text>App</Text>
